427860612f5151f9ceb4b0342a181f356d864fef
Juan Miranda Cárdenas <juan.miranda.c@usach.cl>
2023-03-29 00:18:20 -0300

    ProviderService --> autowired

--- src/main/java/com/mingeso/milkstgomono/controller/ProviderController.java
+++ src/main/java/com/mingeso/milkstgomono/controller/ProviderController.java
@@ -1,5 +1,7 @@
 package com.mingeso.milkstgomono.controller;
 
+import com.mingeso.milkstgomono.repository.ProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class ProviderController {
+    @Autowired
+    ProviderService providerService;
 
     @GetMapping
     public String index(){
---
+++ src/main/java/com/mingeso/milkstgomono/repository/ProviderService.java
@@ -0,0 +1,7 @@
+package com.mingeso.milkstgomono.repository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProviderService {
+}

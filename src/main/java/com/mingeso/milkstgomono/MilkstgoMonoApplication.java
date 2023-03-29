70cb7fdfc69a6655d2458f9c220e1888b19ec189
Juan Miranda Cárdenas <juan.miranda.c@usach.cl>
2023-03-28 23:18:23 -0300

    first commit

---
+++ src/main/java/com/mingeso/milkstgomono/MilkstgoMonoApplication.java
@@ -0,0 +1,13 @@
+package com.mingeso.milkstgomono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkstgoMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkstgoMonoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mingeso/milkstgomono/MilkstgoMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingeso.milkstgomono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkstgoMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

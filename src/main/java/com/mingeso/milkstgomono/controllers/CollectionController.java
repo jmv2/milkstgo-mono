ab277c155f03ff8d092a10bff7341fb788768638
Juan Miranda Cardenas <juan.miranda.c@usach.cl>
2023-04-13 06:32:02 -0400

    update all

--- src/main/java/com/mingeso/milkstgomono/controllers/CollectionController.java
+++ src/main/java/com/mingeso/milkstgomono/controllers/CollectionController.java
@@ -1,7 +1,24 @@
 package com.mingeso.milkstgomono.controllers;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 
 @Controller
+@RequestMapping
 public class CollectionController {
+
+
+    @GetMapping("/importar-acopio")
+    public String importForm() {
+        return "acopio";
+    }
+
 }
